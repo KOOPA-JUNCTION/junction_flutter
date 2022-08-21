6c693a78af013b73a79f19bff469ccefad2b13c8
Minyong Oh <dhalsdyd@gmail.com>
2022-08-21 10:33:03 +0900

    FEAT : api setting

--- lib/app/data/provider/api.dart
+++ lib/app/data/provider/api.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:dio/dio.dart';
 import 'package:firebase_getx_boilerplate/app/data/provider/api_interface.dart';
 
@@ -26,8 +28,42 @@ class FGBPApiProvider implements FGBPApiInterface {
   }
 
   @override
-  Future<void> getInfo() async {
-    String url = "/test";
-    dio.post(url);
+  Future<void> loginWithWallet(String address,
+      {String type = "hex, bech32, base58"}) async {
+    String url = "/auth/wallet";
+    Map data = {"address": address, "type": type};
+    final response = dio.post(url, data: data);
+
+    return;
+  }
+
+  @override
+  Future<void> getNftList() async {
+    String url = "/nfts";
+    dio.get(url);
+  }
+
+  @override
+  Future<void> getNftRank() async {
+    String url = "/nfts/rank";
+    dio.get(url);
+  }
+
+  @override
+  Future<void> search(String query, String offset) async {
+    String url = "/search";
+    dio.get(url, queryParameters: {
+      "query": query,
+      "offset": offset,
+    });
+    return;
+  }
+
+  @override
+  Future<void> uploadImage(String image) async {
+    Map data = {"image": image};
+    String url = "/upload/image";
+    dio.post(url, data: data);
+    return;
   }
 }
--- lib/app/data/provider/api_interface.dart
+++ lib/app/data/provider/api_interface.dart
@@ -1,3 +1,7 @@
 abstract class FGBPApiInterface {
-  Future<void> getInfo();
+  Future<void> loginWithWallet(String address, {String type});
+  Future<void> getNftList();
+  Future<void> getNftRank();
+  Future<void> search(String query, String offset);
+  Future<void> uploadImage();
 }
